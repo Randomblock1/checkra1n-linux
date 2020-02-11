edfe542af2ecde76d3802eb97804c5609be393df
Wilson <33164598+WilsontheWolf@users.noreply.github.com>
2020-02-11 13:00:43 -0700

    Install usbmuxd
    Not tested but should work
    fixes #2

--- repoinstall.sh
+++ repoinstall.sh
@@ -5,4 +5,6 @@ sudo apt-key adv --fetch-keys https://assets.checkra.in/debian/archive.key
 sudo apt update
 echo "Installing..."
 sudo apt install checkra1n
+echo "Installing dependencies..."
+sudo apt install usbmuxd
 echo "All done!"
