cb86ed7a875c7849e213b896d061ec2d9079c946
Benjamin G <therealrandomblock1@gmail.com>
2020-02-22 16:50:47 -0600

    add complete GUI
    woo! v1.0

--- README.md
+++ README.md
@@ -5,12 +5,11 @@ Tested on: multiple VMs and some RPis
 By @randomblock1_
 
 ## how to use
-If you are on a Debian based system *and* running x86_64, use repoinstall. This will install the repo and install checkra1n from that repo.
+Installer.sh is probably the best script for you. Simply run ```./installer.sh``` in bash. If you are on a Debian based system *and* running x86_64, use the "install repo" option in the GUI. This will install the repo and install checkra1n from that repo.
 
-If you are on any other system or any other architecture, use getcheckra1n. This downloads the correct binary and lets you put it in /usr/bin. The repo only contains x86_64 files, which is why this script is needed.
+If you are on any other system or any other architecture, use "direct download" in the GUI. This downloads the correct binary and lets you put it in /usr/bin. The repo only contains x86_64 files, which is why this script is needed.
 
 Please put any issues in the GitHub Issue tracker. Feel free to make pull requests.
 
 ## TODO
 TODO: dynamically fetch the download URLs from the website instead of hardcoding them
-TODO: use whiptail to make dialog boxes instead of standard (y/n) input
--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Checkra1n Easy Installer
 # GitHub Repository: https://github.com/Randomblock1/Checkra1n-Linux
-VERSION=1.0a
+VERSION=1.0
 # Terminal colors
 BLACK=$(tput setaf 0)
 RED=$(tput setaf 1)
@@ -23,136 +23,154 @@ Print_Style () {
   printf "%s\n" "${2}$1${NORMAL}"
 }
 
-if [ "$EUID" -ne 0 ]; then 
-  whiptail --msgbox "YOU AREN'T RUNNING AS # ROOT! This script needs root, use sudo!" 10 30 --ok-button "Exit"
+if [ "$BASH_VERSION" = '' ]; then
+  whiptail --msgbox "ERROR: this script must be run in bash!" 20 30 --ok-button "Exit"
   exit
 fi
 
-if [ "$BASH_VERSION" = '' ]; then
-  whiptail "Warning: this script must be run in bash!" 10 30 --ok-button "Exit"
+if [ "$EUID" -ne 0 ]; then 
+  whiptail --msgbox "YOU AREN'T RUNNING AS ROOT! This script needs root, use sudo!" $((LINES/2)) $((COLUMNS*7/10)) --ok-button "Exit"
+  Print_Style "ERROR: You need to run this as root, use sudo!" $RED
   exit
 fi
 
 LINES=$(tput lines)
 COLUMNS=$(tput cols)
-LISTHEIGHT=$((LINES-8))
-CHOICE=$(whiptail --title "Checkra1n GUI Installer on $(uname -m)" --menu "Choose an option" $((LINES-20)) $((COLUMNS-20)) $((LISTHEIGHT-30)) \
-"Install Repo" "Install the repo. x86_64 ONLY!" \
-"Direct Download" "Use on any architecture." \
-"Credits" "This tool is open-source!" 3>&1 1>&2 2>&3)
+LISTHEIGHT=$((LINES/3))
 
-# this is an excellent guide to whiptail btw https://www.bradgillap.com/guide/post/bash-gui-whiptail-menu-tutorial-series-1
+function mainMenu() {
+  CHOICE=$(whiptail \
+  --title "Checkra1n GUI Installer on $(uname -m)" \
+  --menu "Choose an option" $((LINES/2)) $((COLUMNS*7/10)) $((LISTHEIGHT)) \
+    "Install Repo" "Install the repo. x86_64 ONLY!" \
+    "Direct Download" "Use on any architecture." \
+    "Credits" "This tool is open-source!" \
+    "Update" "Download latest version" 3>&1 1>&2 2>&3)
+  case $CHOICE in
+    "Install Repo")
+    echo "Adding repo..."
+    echo "deb https://assets.checkra.in/debian /" | sudo tee -a /etc/apt/sources.list
+    sudo apt-key adv --fetch-keys https://assets.checkra.in/debian/archive.key
+    sudo apt update
+    echo "Installing..."
+    sudo apt install checkra1n
+    echo "All done!"
+    ;;
+    "Direct Download")
+    # Downloads checkra1n
+    GetJB () {
+      wget "$DL_LINK"
+      chmod 755 checkra1n
+    }
 
-if [ "$CHOICE" = "Direct Download" ]; then
+    # Check system architecture
+    CPUArch=$(uname -m)
+    Print_Style "System Architecture: $CPUArch" "$YELLOW"
 
-# Downloads checkra1n
-GetJB () {
-  wget "$DL_LINK"
-  chmod 755 checkra1n
-}
+    # Get Linux distribution
+    # Copied from Stack Overflow lol
+    if [ -f /etc/os-release ]; then
+        # freedesktop.org and systemd
+        . /etc/os-release
+        OS=$NAME
+        VER=$VERSION_ID
+    elif type lsb_release >/dev/null 2>&1; then
+        # linuxbase.org
+        OS=$(lsb_release -si)
+        VER=$(lsb_release -sr)
+    elif [ -f /etc/lsb-release ]; then
+        # For some versions of Debian/Ubuntu without lsb_release command
+        . /etc/lsb-release
+        OS=$DISTRIB_ID
+        VER=$DISTRIB_RELEASE
+    elif [ -f /etc/debian_version ]; then
+        # Older Debian/Ubuntu/etc.
+        OS=Debian
+        VER=$(cat /etc/debian_version)
+    elif [ -f /etc/SuSe-release ]; then
+        # Older SuSE/etc.
+        ...
+    elif [ -f /etc/redhat-release ]; then
+        # Older Red Hat, CentOS, etc.
+        ...
+    else
+        # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+        OS=$(uname -s)
+        VER=$(uname -r)
+    fi
 
-# Check system architecture
-CPUArch=$(uname -m)
-Print_Style "System Architecture: $CPUArch" "$YELLOW"
+    # Determine Linux distro dependencies
 
-# Get Linux distribution
-# Stolen from Stack Overflow lol
-if [ -f /etc/os-release ]; then
-    # freedesktop.org and systemd
-    . /etc/os-release
-    OS=$NAME
-    VER=$VERSION_ID
-elif type lsb_release >/dev/null 2>&1; then
-    # linuxbase.org
-    OS=$(lsb_release -si)
-    VER=$(lsb_release -sr)
-elif [ -f /etc/lsb-release ]; then
-    # For some versions of Debian/Ubuntu without lsb_release command
-    . /etc/lsb-release
-    OS=$DISTRIB_ID
-    VER=$DISTRIB_RELEASE
-elif [ -f /etc/debian_version ]; then
-    # Older Debian/Ubuntu/etc.
-    OS=Debian
-    VER=$(cat /etc/debian_version)
-elif [ -f /etc/SuSe-release ]; then
-    # Older SuSE/etc.
-    ...
-elif [ -f /etc/redhat-release ]; then
-    # Older Red Hat, CentOS, etc.
-    ...
-else
-    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
-    OS=$(uname -s)
-    VER=$(uname -r)
-fi
-
-# Determine Linux distro
-
-if [[ "$OS" == *"Raspbian"* ]]; then
-  DEPENDENCIES="usbmuxd libimobiledevice6"
+    if [[ "$OS" == *"Raspbian"* ]]; then
+      DEPENDENCIES="usbmuxd libimobiledevice6"
   
-else
-  Print_Style "I do not know what dependencies you need for this distro ($OS). Using defaults for Raspbian..." "$RED"
-  DEPENDENCIES="usbmuxd libimobiledevice6"
-fi
+    else
+      Print_Style "I do not know what dependencies you need for this distro ($OS). Using defaults for Raspbian..." "$RED"
+      DEPENDENCIES="usbmuxd libimobiledevice6"
+    fi
 
-# Choose correct download link
-# TODO: dynamically fetch latest urls from checkra1n website
-if [[ "$CPUArch" == *"aarch64"* || "$CPUArch" == *"arm64"* ]]; then
-  Print_Style "ARM64 detected!" "$YELLOW"
-  DL_LINK=https://assets.checkra.in/downloads/linux/cli/arm64/0a640fd52276d5640bbf31c54921d1d266dc2303c1ed26a583a58f66a056bfea/checkra1n
+    # Choose correct download link
+    # TODO: dynamically fetch latest urls from checkra1n website
+    if [[ "$CPUArch" == *"aarch64"* || "$CPUArch" == *"arm64"* ]]; then
+      Print_Style "ARM64 detected!" "$YELLOW"
+      DL_LINK=https://assets.checkra.in/downloads/linux/cli/arm64/0a640fd52276d5640bbf31c54921d1d266dc2303c1ed26a583a58f66a056bfea/checkra1n
   
-elif [[ "$CPUArch" == *"armhf"* || "$CPUArch" == *"armv"* ]]; then
-  Print_Style "ARM detected!" "$YELLOW"
-  DL_LINK=https://assets.checkra.in/downloads/linux/cli/arm/5f7d4358971eb2823413801babbac0158524da80c103746e163605d602ff07bf/checkra1n
+    elif [[ "$CPUArch" == *"armhf"* || "$CPUArch" == *"armv"* ]]; then
+      Print_Style "ARM detected!" "$YELLOW"
+      DL_LINK=https://assets.checkra.in/downloads/linux/cli/arm/5f7d4358971eb2823413801babbac0158524da80c103746e163605d602ff07bf/checkra1n
   
-elif [[ "$CPUArch" == *"x86_64"* ]]; then
-  Print_Style "x86_64 detected!" "$YELLOW"
-  DL_LINK=https://assets.checkra.in/downloads/linux/cli/x86_64/eda98d55f500a9de75aee4e7179231ed828ac2f5c7f99c87442936d5af4514a4/checkra1n
+    elif [[ "$CPUArch" == *"x86_64"* ]]; then
+      Print_Style "x86_64 detected!" "$YELLOW"
+      DL_LINK=https://assets.checkra.in/downloads/linux/cli/x86_64/eda98d55f500a9de75aee4e7179231ed828ac2f5c7f99c87442936d5af4514a4/checkra1n
 
-elif [[ "$CPUArch" == *"x86"* ]]; then
-  Print_Style "x86 detected!" "$YELLOW"
-  DL_LINK=https://assets.checkra.in/downloads/linux/cli/i486/26952e013ece4d0e869fc9179bfd2b1f6c319cdc707fadf44fdb56fa9e62f454/checkra1n
+    elif [[ "$CPUArch" == *"x86"* ]]; then
+      Print_Style "x86 detected!" "$YELLOW"
+      DL_LINK=https://assets.checkra.in/downloads/linux/cli/i486/26952e013ece4d0e869fc9179bfd2b1f6c319cdc707fadf44fdb56fa9e62f454/checkra1n
 
-else
-  Print_Style "ERROR: Unknown/Unsuported architecture! Please try again, make sure your architecture is supported by checkra1n and that you are using sh instead of bash." "$RED"
-  DL_LINK=UNKNOWN
-  exit
-fi
+    else
+      Print_Style "ERROR: Unknown/Unsuported architecture! Make sure your architecture is supported by checkra1n." "$RED"
+      DL_LINK=UNKNOWN
+      exit
+    fi
 
-Print_Style "Getting checkra1n..." "$GREEN"
-GetJB
-Print_Style "Done! Marked as executable!" "$GREEN"
-
-Print_Style "Install to /usr/bin (y/n?)" "$YELLOW"
-  read answer
-  if [ "$answer" != "${answer#[Yy]}" ]; then
-    cp checkra1n /usr/bin
-    Print_Style "Copied executable to /usr/bin" "$GREEN"
-  Print_Style "Delete downloaded file (no longer needed)? (y/n)" "$YELLOW"
+    Print_Style "Getting checkra1n..." "$GREEN"
+    GetJB
+    Print_Style "Done! Marked as executable!" "$GREEN"
+    Print_Style "Install to /usr/bin (y/n?)" "$YELLOW"
     read answer
     if [ "$answer" != "${answer#[Yy]}" ]; then
-    rm checkra1n
+      cp checkra1n /usr/bin
+      Print_Style "Copied executable to /usr/bin" "$GREEN"
+      Print_Style "Delete downloaded file (no longer needed)? (y/n)" "$YELLOW"
+      read answer
+        if [ "$answer" != "${answer#[Yy]}" ]; then
+          rm checkra1n
+        fi
     fi
-  fi
-Print_Style "Attenpting to install dependencies." "$BLUE"
-# TODO: detect if yum or others are needed
-  apt install -y "$DEPENDENCIES"
-Print_Style "All done!" "$BLUE"
-
-elif [ "$CHOICE" = "Install Repo" ]; then
-echo "Adding repo..."
-echo "deb https://assets.checkra.in/debian /" | sudo tee -a /etc/apt/sources.list
-sudo apt-key adv --fetch-keys https://assets.checkra.in/debian/archive.key
-sudo apt update
-echo "Installing..."
-sudo apt install checkra1n
-echo "All done!"
-
-elif [ "$CHOICE" = "Credits" ]; then
-whiptail --title "Checkra1n GUI Installer" --msgbox "Checkra1n GUI Installer made by Randomblock1.\nThis project is open source! Check out https://github.com/Randomblock1/Checkra1n-Linux! \nFollow me on Twitter @randomblock1_! \nPlease report all bugs in the GitHub issue tracker and feel free to make pull requests! \nINFO: $OS $(uname -mo) \nVERSION: $VERSION" $((LINES-20)) $((COLUMNS-20)) $((LISTHEIGHT-30))
-
-else
-echo "nothing selected"
-fi
+    Print_Style "Attenpting to install dependencies." "$BLUE"
+    # TODO: detect if yum or others are needed
+    apt install -y "$DEPENDENCIES"
+    Print_Style "All done!" "$BLUE"
+    ;;
+    "Credits")
+    whiptail --title "Checkra1n GUI Installer" --msgbox "Checkra1n GUI Installer made by Randomblock1.\nThis project is open source! Check out https://github.com/Randomblock1/Checkra1n-Linux! \nFollow me on Twitter @randomblock1_! \nPlease report all bugs in the GitHub issue tracker and feel free to make pull requests! \nINFO: $OS $(uname -mo) \nVERSION: $VERSION" $((LINES/2)) $((COLUMNS*7/10)) $((LISTHEIGHT))
+    mainMenu
+    ;;
+    "Update")
+    whiptail --title "Checkra1n GUI Installer" --yesno "Update to latest version?" $((LINES/2)) $((COLUMNS*7/10))
+    case $? in
+      1)
+      mainMenu
+      ;;
+      0)
+      git clone http://github.com/randomblock1/checkra1n-linux
+      cd checkra1n-linux
+      chmod 755 *.sh
+      mv -f * ..
+      cd ..
+      rm -R checkra1n-linux
+     ./installer.sh
+   esac
+  esac
+}
+mainMenu
