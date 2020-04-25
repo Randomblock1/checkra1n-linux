002c2e089a97e8672534667993e09d334e8faa3f
Benjamin G <therealrandomblock1@gmail.com>
2020-04-25 14:55:28 -0500

    big update
    Removes unused scripts and adds support for autostarting checkra1n on
    boot

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ download without installing using bash: ```curl -s https://raw.githubusercontent
 (note: selecting the update option will install it to your machine)
 
 ## how to use
-Use the "Install Repo" option if you are on x86_64. Otherwise, use "Direct Download". "Credits" is self explanatory, and "Update" uses git to get the latest version of this tool and installs it.
+Use the "Install Repo" option if you are on x86_64. Otherwise, use "Direct Download", which will install checkra1n to /usr/bin if you tell it to. "Credits" is self explanatory, and "Update" uses wget to get the latest version of this tool. “Install Autostart Service” will automatically start checkra1n on boot. There are 2 options for it: checkra1n and webra1n. Checkra1n will jailbreak your devices automatically with the downside of having to manually put your device into DFU mode (instructions here: https://www.reddit.com/r/jailbreak/wiki/dfumode) before jailbreaking. Webra1n requires you to connect to the device via WiFi or Ethernet. Both of these options cannot be used together because I’m pretty sure checkra1n interferes with other instances of itself.
 
 If you are not on a Debian based system, you may need to install additional dependencies. Just google any errors and see if it means you're missing something.
 
--- installer.sh
+++ installer.sh
@@ -38,16 +38,26 @@ LINES=$(tput lines)
 COLUMNS=$(tput cols)
 LISTHEIGHT=$((LINES/3))
 
+# get system architecture
+CPUArch=$(uname -m)
+
 function mainMenu() {
   CHOICE=$(whiptail \
   --title "Checkra1n GUI Installer on $(uname -m)" \
   --menu "Choose an option" $((LINES/2)) $((COLUMNS*7/10)) $((LISTHEIGHT)) \
     "Install Repo" "Install the repo. x86_64 ONLY!" \
     "Direct Download" "Use on any architecture." \
+    "Install Autostart Service" "Automatically start checkra1n on boot." \
     "Credits" "This tool is open-source!" \
     "Update/Reinstall" "Download latest version" 3>&1 1>&2 2>&3)
   case $CHOICE in
     "Install Repo")
+    if [[ "$CPUArch" == *"x86_64"* ]]; then
+      Print_Style "x86_64 detected!" $GREEN
+    else
+      Print_Style "ERROR:You aren't on x86_64! You can't use this! Exiting..." $RED
+      exit
+    fi
     echo "Adding repo..."
     echo "deb https://assets.checkra.in/debian /" | sudo tee -a /etc/apt/sources.list
     sudo apt-key adv --fetch-keys https://assets.checkra.in/debian/archive.key
@@ -64,7 +74,6 @@ function mainMenu() {
     }
 
     # Check system architecture
-    CPUArch=$(uname -m)
     Print_Style "System Architecture: $CPUArch" "$YELLOW"
 
     # Get Linux distribution
@@ -135,6 +144,7 @@ function mainMenu() {
     Print_Style "Getting checkra1n..." $GREEN
     GetJB
     Print_Style "Done! Marked as executable!" $GREEN
+    # Ask user if they want it copied to /usr/bin for easy $PATH access
     whiptail --yesno "Install checkra1n to /usr/bin/ so you can execute it anywhere?" $((LINES/2)) $((COLUMNS*7/10)) $((LISTHEIGHT))
     if [ "$?" != "0" ]; then
       cp checkra1n /usr/bin
@@ -149,8 +159,50 @@ function mainMenu() {
     apt install -y $DEPENDENCIES
     Print_Style "All done!" $BLUE
     ;;
+    "Install Autostart Service")
+    CHOICE=$(whiptail \
+  --title "Checkra1n Service Installer on $(uname -m)" \
+  --menu "Choose ONE OR THE OTHER. Having them both working is not possible since they interfere with each other (i think?)." $((LINES/2)) $((COLUMNS*7/10)) $((LISTHEIGHT)) \
+    "Install Automatic checkra1n" "Automatically jailbreaks DFU devices." \
+    "Install Automatic webra1n" "Starts webra1n on port 8081." 3>&1 1>&2 2>&3)
+    case $CHOICE in
+    "Install Automatic checkra1n")
+      whiptail --yesno "Install autostart service? This requires you to put your device into DFU mode manually it to work." $((LINES/2)) $((COLUMNS*7/10)) $((LISTHEIGHT))
+      if [ "$?" != "0" ]; then
+        wget -O checkra1n-linux.service https://raw.githubusercontent.com/Randomblock1/Checkra1n-Linux/master/checkra1n-linux.service
+        chmod 644 checkra1n-linux.service
+        mv checkra1n-linux.service /lib/systemd/system/checkra1n-linux.service
+        Print_Style "Moved service to /lib/systemd/system" $GREEN
+        Print_Style "Enabling service..." $GREEN
+        systemctl daemon-reload
+        systemctl enable checkra1n-linux
+        systemctl restart checkra1n-linux
+        Print_Style "Success!"$ $GREEN
+        whiptail --title "Using Checkra1n Autostart Service" --msgbox "This installation is now configured to autostart checkra1n on boot. \nThis means that your device must be in DFU mode manually in order to jailbreak. The autostart service may also interfere with other instances of checkra1n and cause them to fail. \nInstructions of how to put your device into DFU mode are here: \nhttps://www.reddit.com/r/jailbreak/wiki/dfumode \nPlug your device in, push some buttons, and checkra1n will do its work."
+      fi
+      mainMenu
+      ;;
+      "Install Automatic webra1n")
+      whiptail --yesno "Install autostart service? This requires you to use a web browser on a different device to jailbreak." $((LINES/2)) $((COLUMNS*7/10)) $((LISTHEIGHT))
+      if [ "$?" != "0" ]; then
+        wget -O checkra1n-linux.service https://raw.githubusercontent.com/Randomblock1/Checkra1n-Linux/master/checkra1n-linux-webra1n.service
+        chmod 644 checkra1n-linux.service
+        mv checkra1n-linux.service /lib/systemd/system/checkra1n-linux.service
+        Print_Style "Moved service to /lib/systemd/system" $GREEN
+        Print_Style "Enabling service..." $GREEN
+        systemctl daemon-reload
+        systemctl enable checkra1n-linux
+        systemctl restart checkra1n-linux
+        Print_Style "Success!"$ $GREEN
+        NET_IP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -1`
+        whiptail --title "Using Checkra1n Autostart Service" --msgbox "This installation is now configured to autostart webra1n on boot. \nThis means that you must use a web browser and access this device's local IP via WiFi or Ethernet at port 8081. \nCurrent local URL: \n$NET_IP:8081/ \nNote: this will change if the device connects to a different network or loses connection."
+      if
+      mainMenu
+    esac
+    ;;
     "Credits")
-    whiptail --title "Checkra1n GUI Installer" --msgbox "Checkra1n GUI Installer made by Randomblock1.\nThis project is open source! Check out https://github.com/Randomblock1/Checkra1n-Linux! \nFollow me on Twitter @randomblock1_! \nPlease report all bugs in the GitHub issue tracker and feel free to make pull requests! \nINFO: $OS $(uname -mo) \nVERSION: $VERSION" $((LINES/2)) $((COLUMNS*7/10)) $((LISTHEIGHT))
+    NET_IP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -1`
+    whiptail --title "Checkra1n GUI Installer" --msgbox "Checkra1n GUI Installer made by Randomblock1.\nThis project is open source! Check out https://github.com/Randomblock1/Checkra1n-Linux! \nFollow me on Twitter @randomblock1_! \nPlease report all bugs in the GitHub issue tracker and feel free to make pull requests! \nINFO: $OS $(uname -mo) \nVERSION: $VERSION \nLocal IP: $NET_IP" $((LINES/2)) $((COLUMNS*7/10)) $((LISTHEIGHT))
     mainMenu
     ;;
     "Update/Reinstall")
@@ -160,12 +212,15 @@ function mainMenu() {
       mainMenu
       ;;
       0)
-      git clone http://github.com/randomblock1/checkra1n-linux
+      Print_Style "Updating..." $GREEN
+      mkdir checkra1n-linux
       cd checkra1n-linux
+      wget https://raw.githubusercontent.com/Randomblock1/Checkra1n-Linux/master/installer.sh
       chmod 755 *.sh
       mv -f * ..
       cd ..
       rm -R checkra1n-linux
+      Print_Style "Completed!" $GREEN
      ./installer.sh
    esac
   esac
